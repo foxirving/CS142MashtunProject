69736ee53fa083176b007ad7ffb6101ae65dd610
Amy Irving <amy.eli.irving@gmail.com>
2015-03-11 20:42:31 -0700

    123

--- src/raspberry/gui/MainGUI.java
+++ src/raspberry/gui/MainGUI.java
@@ -30,8 +30,7 @@ public class MainGUI extends WelcomePanel{
 					MainGUI window = new MainGUI();
 					window.frame.setVisible(true);
 
-					//hahaha
-					//Initialize main window not to be continued!
+					//Initialize main window
 					
 					/**
 					 * This is where we add the new Panel classes.
